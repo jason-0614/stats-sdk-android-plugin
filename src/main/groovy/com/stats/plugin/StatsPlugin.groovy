f57d4d49dc2fe7e18536271d380a7b4338fb74b1
chenglihai <chenglihai@mysteel.com>
2022-10-18 16:12:52 +0800

    创建工程

---
+++ src/main/groovy/com/stats/plugin/StatsPlugin.groovy
@@ -0,0 +1,11 @@
+package com.stats.plugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class StatsPlugin implements Plugin<Project> {
+    @Override
+    void apply(Project project) {
+
+    }
+}
